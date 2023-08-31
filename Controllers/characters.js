337739c2e9d45812723938e40fa57d167dbc8e5a
Savitur Maharaj <saviturmaharaj@gmail.com>
2023-08-31 10:38:01 -0600

    Complete Character CRUD Functionality

--- Controllers/characters.js
+++ Controllers/characters.js
@@ -11,21 +11,64 @@ const createCharacter= async(req, res) => {
 
 // Read
 const getAllCharacters = async(req, res) => {
-    res.send('Get All Characters')
+    const characters = await CharacterModel.find({createdBy: req.user.userId}).sort({updatedAt: -1}) // -1 to show more recent changes at beginning
+    res.status(StatusCodes.OK).json({characters})
 }
 
 const getCharacter= async(req, res) => {
-    res.send('Get singular character')
+    const {id: characterId} = req.params // id param is from route and represents the job id
+    const {userId} = req.user
+
+    const character = await CharacterModel.findOne({
+        _id: characterId,
+        createdBy: userId // Avoid allowing anyone with the job id access to the job
+    })
+
+    if(!character){
+        throw new CantFindError(`Character with id ${characterId} does not exist`)
+    }
+
+    res.status(StatusCodes.OK).json({character})
 }
 
 // Update
 const updateCharacter= async(req, res) => {
-    res.send('Update a character')
+    const {id: characterId} = req.params 
+    const {userId} = req.user
+    const {name, medium, quality} = req.body
+
+    if(name === '' || medium === '' || quality === ''){
+        throw new BadRequestError('Name, medium and quality are all required')
+    }
+
+    const character = await CharacterModel.findOneAndUpdate({
+        _id: characterId,
+        createdBy: userId
+    }, req.body, {new:true, runValidators:true}) // new:true returns the new character
+
+    if(!character){
+        throw new CantFindError(`Character with id ${characterId} does not exist`)
+    }
+
+    res.status(StatusCodes.OK).json({character})
 }
+
 
 // Delete
 const deleteCharacter= async(req, res) => {
-    res.send('Delete a character')
+    const {id: characterId} = req.params 
+    const {userId} = req.user
+
+    const character = await CharacterModel.findByIdAndDelete({
+        _id: characterId,
+        createdBy: userId
+    })
+
+    if(!character){
+        throw new CantFindError(`Character with id ${characterId} does not exist`)
+    }
+
+    res.status(StatusCodes.OK).send()
 }
 
 
