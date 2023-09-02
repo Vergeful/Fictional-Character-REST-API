88283bc754098c02e5e77772de7d17ed6bac8438
Savitur Maharaj <saviturmaharaj@gmail.com>
2023-09-01 23:39:16 -0600

    Add dependencies

--- README.md
+++ README.md
@@ -15,3 +15,13 @@ Users can only access their own character recordings.
 They can create characters, get all characters, get a single character, edit a character's information or delete a character (CRUD).
 
 When getting all characters, users can filter to only access characters of a specific type, quality and/or medium.
+
+Some dependencies and their purpose:
+1. cors - Allows the api to be accessible from different domains (public)
+2. helmet - Sets multiple http headers to prevent attacks
+3. xss-clean - Cleans user info to prevent attacks
+4. express-rate-limit - Sets a request limit
+5. joi - 
+6. rate-limiter
+7. swagger-ui-express
+8. yamljs
--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 
+const { rateLimit } = require('express-rate-limit')
+const cors = require('cors')
+const helmet = require('helmet')
+const xssClean = require('xss-clean')
+
 require('dotenv').config() // allows access to .env (local environment) variables via process.env
 require('express-async-errors') // middleware for errors
 
@@ -14,6 +19,18 @@ const characterRouter = require('./Routes/characters')
 //MIDDLEWARE:
 // Built in middleware function in express that parses incoming requests with JSON payloads:
 app.use(express.json());
+
+// Middleware for security:
+app.set('trust proxy', 1)
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 mins
+    max: 50 // 50 requests can be made per windowMs
+}))
+
+app.use(cors())
+app.use(helmet())
+app.use(xssClean())
+app.use(rateLimit())
 
 // Middleware if there is an error in finding page (404 status code):
 const cantFindMiddleware = require('./Middleware/cant-find')
